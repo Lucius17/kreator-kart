6eedaec99164645a91d1754dc81a0a5c1315d96a
Lucius <kwaslukas@gmail.com>
2025-03-27 20:03:19 +0100

    fix

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	darkMode: 'class',
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+	  extend: {
+		fontFamily: {
+        'comic-sans': ['"Comic Sans MS"', 'cursive']
+      },
+		spacing: {
+		  '496': '496px',
+		  '693': '693px'
+		}
+	  }
+	},
+	plugins: [],
+  }
